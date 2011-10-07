251f6cc5dc74b0f37bd5ceab9895ebe100df01cd
Josh Stone <cuviper@gmail.com>
2011-10-07 16:59:20 -0700

    Update for GNOME 3.2
    http://blog.cuviper.com/2011/10/07/force-zoomed-workspaces-in-gnome-3-2/

--- extension.js
+++ extension.js
@@ -1,9 +1,22 @@
 const Overview = imports.ui.main.overview;
 const WorkspacesView = imports.ui.workspacesView;
 
-function main() {
-    WorkspacesView.WorkspacesDisplay.prototype._updateAlwaysZoom = function() {
+function init(metadata) {}
+
+let _saved_updateAlwaysZoom;
+
+function enable() {
+    let p = WorkspacesView.WorkspacesDisplay.prototype;
+    _saved_updateAlwaysZoom = p._updateAlwaysZoom;
+    p._updateAlwaysZoom = function() {
         this._alwaysZoomOut = true;
     };
     Overview._workspacesDisplay._alwaysZoomOut = true;
+}
+
+function disable() {
+    let p = WorkspacesView.WorkspacesDisplay.prototype;
+    p._updateAlwaysZoom = _saved_updateAlwaysZoom;
+    Overview._workspacesDisplay._alwaysZoomOut = false;
+    Overview._workspacesDisplay._updateAlwaysZoom();
 }
